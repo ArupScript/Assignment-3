bb2ab99aa70c2da3b18f5246b6f40ac894584733
ArupScript <arupm22@gmail.com>
2025-08-01 17:07:32 +0530

    Add files via upload

---
+++ Assignment 3, Task 2.py
@@ -0,0 +1,11 @@
+#Task 2
+import math
+num = float(input("Enter a number: "))
+
+square = math.sqrt(num)
+logr = math.log(num)
+sine = math.sin(num)
+
+print("Square root:", square)
+print("Logarithm:", logr)
+print("Sine:", sine)
